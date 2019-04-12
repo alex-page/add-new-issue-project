219675334e4ae5e9fa1320196052c67be4c2fd82
Alex Page <alex@alexpage.com.au>
2019-04-12 09:43:56 -0400

    add project as argument

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
 const { Toolkit } = require( 'actions-toolkit' );
 
 
-const PROJECT_FRAGMENT = `
-  name
-  id
-  columns(first: 10) {
-    totalCount
-    nodes {
-      id
-      url
-      firstCards: cards(first: 1) {
-        totalCount
-        nodes {
-          url
-          id
-          note
-        }
-      }
-      lastCards: cards(last: 1) {
-        totalCount
-        nodes {
-          url
-          id
-          note
-        }
-      }
-    }
-  }
-`;
-
-
 Toolkit.run(async ( tools ) => {
   const url = tools.context.payload.issue 
     ? tools.context.payload.issue.html_url 
@@ -39,36 +10,7 @@ Toolkit.run(async ( tools ) => {
   
   tools.log( `Adding ${ url } to project` );
 
-
-  const { resource } = tools.github.graphql( `
-      query getAllProjectCards( $url: URI! ) {
-        resource( url: $url ) {
-          ... on Issue {
-            id
-            repository {
-              owner {
-                url
-                ${''/* Projects can be attached to an Organization... */}
-                ... on Organization {
-                  projects(first: 10, states: [OPEN]) {
-                    nodes {
-                      ${ PROJECT_FRAGMENT }
-                    }
-                  }
-                }
-              }
-              ${''/* ... or on a Repository */}
-              projects(first: 10, states: [OPEN]) {
-                nodes {
-                  ${ PROJECT_FRAGMENT }
-                }
-              }
-            }
-          }
-        }
-      }
-    `, { url });
-
+  tools.log( tools.arguments );
 
   tools.log.success( 'Hello world 🦁' );
 }, {
