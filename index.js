39c8a3aa7e103fa69920d18dd549a8cb9e9a6c2c
Alex Page <alex@alexpage.com.au>
2019-04-12 09:15:48 -0400

    Change to opened

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const { Toolkit } = require( 'actions-toolkit' );
 Toolkit.run(async tools => {
   tools.log.success( 'Hello world 🦁' )
 }, {
-  event: [ 'issues.created' ],
+  event: [ 'issues.opened' ],
   secrets: [ 'GITHUB_TOKEN' ],
 })
