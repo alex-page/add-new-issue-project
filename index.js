19afb168119ac7ac172499024004df1338b22cbf
Alex Page <alex@alexpage.com.au>
2019-04-12 09:36:40 -0400

    Adding some graphql

--- index.js
+++ index.js
@@ -1,9 +1,75 @@
 const { Toolkit } = require( 'actions-toolkit' );
 
 
+const PROJECT_FRAGMENT = `
+  name
+  id
+  columns(first: 10) {
+    totalCount
+    nodes {
+      id
+      url
+      firstCards: cards(first: 1) {
+        totalCount
+        nodes {
+          url
+          id
+          note
+        }
+      }
+      lastCards: cards(last: 1) {
+        totalCount
+        nodes {
+          url
+          id
+          note
+        }
+      }
+    }
+  }
+`;
+
+
 Toolkit.run(async ( tools ) => {
-  tools.log( tools.context.issue );
-  tools.log( tools.context.repo );
+  const url = tools.context.payload.issue 
+    ? tools.context.payload.issue.html_url 
+    : tools.context.payload.pull_request.html_url;
+
+  tools.log( tools.context.payload );
+  
+  tools.log( `Adding ${ url } to project` );
+
+
+  const { resource } = tools.github.graphql( `
+      query getAllProjectCards( $url: URI! ) {
+        resource( url: $url ) {
+          ... on Issue {
+            id
+            repository {
+              owner {
+                url
+                ${''/* Projects can be attached to an Organization... */}
+                ... on Organization {
+                  projects(first: 10, states: [OPEN]) {
+                    nodes {
+                      ${ PROJECT_FRAGMENT }
+                    }
+                  }
+                }
+              }
+              ${''/* ... or on a Repository */}
+              projects(first: 10, states: [OPEN]) {
+                nodes {
+                  ${ PROJECT_FRAGMENT }
+                }
+              }
+            }
+          }
+        }
+      }
+    `, { url });
+
+
   tools.log.success( 'Hello world 🦁' );
 }, {
   event: [ 'issues.opened', 'pull_request.opened' ],
