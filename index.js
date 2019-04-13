2bf7a79fbf4cf6c3d982c17d892f89df3b1d51ed
Alex Page <alex@alexpage.com.au>
2019-04-13 14:15:50 -0400

    Add mutation to move project to board

--- index.js
+++ index.js
@@ -2,28 +2,23 @@ const { Toolkit } = require( 'actions-toolkit' );
 
 
 Toolkit.run(async ( tools ) => {
-  tools.log( tools.arguments );
+  // Get the arguments
   const projectNumber = tools.arguments._[ 0 ];
-  const projectColumn = tools.arguments._[ 1 ];
+  const columnName    = tools.arguments._[ 1 ];
 
-  // tools.log( tools.context.payload );
+  // Get the data from the event
+  const issueUrl   = tools.context.payload.issue.html_url;
+  const issueTitle = tools.context.payload.issue.title;
+  const issueId    = tools.context.payload.issue.id;
 
-  tools.log( `Project number arg: ${ projectNumber }` );
-  tools.log( `Project column arg: ${ projectColumn }` );
-
-  // Get the current issue or pullrequest URL
-  const url = tools.context.payload.issue 
-    ? tools.context.payload.issue.html_url 
-    : tools.context.payload.pull_request.html_url;
-
-  // Get the issue ID, project ID and the column ID
+  // Get the project ID and the column ID
   const { resource } = await tools.github.graphql(`query {
-    resource( url: "${ url }" ) {
+    resource( url: "${ issueUrl }" ) {
       ... on Issue {
-        id
         repository {
           projects( first: 10, states: [OPEN] ) {
             nodes {
+              name
               number
               id
               columns( first: 10 ) {
@@ -39,6 +34,7 @@ Toolkit.run(async ( tools ) => {
             ... on Organization {
               projects( first: 10, states: [OPEN] ) {
                 nodes {
+                  name
                   number
                   id
                   columns( first: 10 ) {
@@ -54,40 +50,35 @@ Toolkit.run(async ( tools ) => {
         }
       }
     }
-  }`);
-
-  resource.repository.projects.nodes.forEach( node => tools.log( node.number, projectNumber, typeof node.number, typeof projectNumber ) );
-
-  const issueId = resource.id;
-  tools.log( `Issue ID: ${ issueId }`) ;
+  }`)
 
   const project = resource.repository.projects.nodes
     .filter( node => node.number === projectNumber )[ 0 ];
   const projectId = project.id;
-  tools.log( `Project ID: ${ project }` );
-  tools.log( `Project ID: ${ projectId }` );
 
-  const projectColumnId = project.columns.nodes.filter( node => node.name == projectColumn );
-  tools.log( `Project column ID: ${ projectColumnId }` );
+  const column = project.columns.nodes.filter( node => node.name == columnName )[ 0 ];
+  const columnId = column.id;
 
-
-
-
-  if( !projectColumnId ){
-    tools.exit.failure( `Could not find column for "${ projectColumn }"` );
+  // Check we have a valid column ID
+  if( !columnId ){
+    tools.exit.failure( `Could not find column for "${ columnName }"` );
   }
 
+  // Check we have a valid project ID
   if( !projectId ){
     tools.exit.failure( `Could not find project that has the number "${ projectNumber }"` );
   }
 
-  tools.log( `Issue ID: ${ issueId }`);
-  tools.log( `Project ID: ${ projectId }`);
-  tools.log( `Project column ID: ${ projectColumnId }`);
+  // Add the card to the project
+  await tools.github.graphql(`
+    mutation createCard( contentId: ${ issueId }, columnId: ${ columnId } ) {
+      addProjectCard( input: { contentId: $contentId, projectColumnId: $columnId }) {
+        clientMutationId
+      }
+    }
+  `);
 
-  tools.log( `Adding ${ url } to project` );
-
-  tools.log.success( 'Hello world 🦁' );
+  tools.log.success( `Added issue ${ issueTitle } to project ${ project.name } in column ${ column.name }` );
 }, {
   event: [ 'issues.opened', 'pull_request.opened' ],
   secrets: [ 'GITHUB_TOKEN' ],
