e960218f74da5c0db7dc57bb6b62be8c4e6bc4af
Alex Page <alex@alexpage.com.au>
2019-04-13 20:27:01 -0400

    Search project names

--- index.js
+++ index.js
@@ -4,41 +4,75 @@ const { Toolkit } = require( 'actions-toolkit' );
 Toolkit.run( async ( tools ) => {
   try {
     // Get the arguments
-    const projectNumber = tools.arguments._[ 0 ];
-    const columnName    = tools.arguments._[ 1 ];
+    const projectName = tools.arguments._[ 0 ];
+    const columnName  = tools.arguments._[ 1 ];
 
     // Get the data from the event
-    const issueUrl   = tools.context.payload.issue.html_url;
-    const issueTitle = tools.context.payload.issue.title;
-    const issueId    = tools.context.payload.issue.node_id;
+    const { issue } = tools.context.payload.issue;
 
-    tools.log( tools.context.payload );
+    // Get the project ID from the name
+    const { resource } = await tools.github.graphql(`query {
+      resource( url: "${ issue.url }" ) {
+        ... on Issue {
+          repository {
+            projects( search: "${ projectName }", first: 1, states: [OPEN] ) {
+              nodes {
+                columns( first: 100 ) {
+                  nodes {
+                    id
+                    name
+                  }
+                }
+              }
+            }
+            owner {
+              ... on Organization {
+                projects( search: "${ projectName }", first: 1, states: [OPEN] ) {
+                  nodes {
+                    columns( first: 100 ) {
+                      nodes {
+                        id
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`);
 
-    // Get the issue id, project name and number, column ID and name
-    // New code goes here
+    // Get the closest matching array of columns
+    const repoProjectColumns = resource.repository.project.nodes.length
+      ? resource.repository.project.nodes[ 0 ].columns
+      : [];
 
-    // Get the project from the matching provided number
-    const project = resource.repository.projects.nodes
-      .filter( node => node.number === projectNumber )[ 0 ];
+    const orgProjectColumns = resource.repository.owner.project.nodes.length 
+      ? resource.repository.owner.project.nodes[ 0 ].columns
+      : [];
 
     // Get the column from the matching provided column name
-    const column = project.columns.nodes.filter( node => node.name === columnName )[ 0 ];
-    const columnId = column.id;
+    const columns = [ ...repoProjectColumns, ...orgProjectColumns ].nodes
+      .filter( node => node.name === columnName );
 
     // Check we have a valid column ID
-    if( !columnId || !project ) {
-      tools.exit.failure(
-        `Could not find project number "${ projectNumber }" or column "${ columnName }"`
-      );
+    if( !columns.length ) {
+      tools.exit.failure( `Could not find "${ projectName }" with "${ columnName }" column` );
     }
 
     // Add the card to the project
-    await tools.github.createCard({ column_id: columnId, content_id: issueId, content_type: "Issue" });
+    columns.forEach( column => {
+      await tools.github.createCard({ 
+        column_id: column.id,
+        content_id: issue.node_id,
+        content_type: "Issue"
+      });
+    });
 
     // Log success message
-    tools.log.success(
-      `Added issue "${ issueTitle }" to "${ project.name }" in "${ column.name }".`
-    );
+    tools.log.success( `Added "${ issue.title }" to "${ projectName }" in "${ columnName }".` );
   }
   catch( error ){
     tools.exit.failure( error );
